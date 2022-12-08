82700e75fa43ead28133342a1a723a92156383db
igor pieruccini <igor.pierucicni@sosafe.de>
2022-12-08 22:12:22 +0100

    Add basically onClock functionality to EditorContainer

--- src/components/EditorContainer/EditorContainer.tsx
+++ src/components/EditorContainer/EditorContainer.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { EsperBlock } from '../sharedTypes';
 import './EditorContainer.scss';
 
 export const EditorContainer = ()=> {
 
+    const [ blocks, setBlocks ] = useState<Array<EsperBlock>>([]);
+
+    const OnClickEditorContainer = ()=> {
+        setBlocks((old)=> [...old, { id: Math.random().toString()}])
+    }
+
     return (
-        <div className='EditorContainer'>
+        <div 
+            className='EditorContainer'
+            onClick={OnClickEditorContainer}
+            >
             Editor container
+            <div>
+                {blocks.map((block)=> <p key={block.id}>new block: {block.id}</p>)}
+            </div>
         </div>
     )
 }
