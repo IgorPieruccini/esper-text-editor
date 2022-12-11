1c59e17e0ba4b9a199d01d2d0af29e7a04cbaa4a
Igor Pieruccini <igor.pieruccini@wooga.net>
2022-12-11 22:23:43 +0100

    Show and close command panel (#2)
    * Identify when user wants to open command panel
    
    * add and show CommandPanel
    
    * render command panel
    
    * Close command on filter attempt
    
    * support multiple command trigger in a line
    
    * close commands if div with command is deleted
    
    Co-authored-by: igor pieruccini <igor.pierucicni@sosafe.de>

---
+++ src/components/CommandPanel/CommandPanel.tsx
@@ -0,0 +1,91 @@
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Vector2 } from '../sharedTypes';
+import './CommandPanel.scss';
+
+/** max of attempts to filter a result before closing command panel */
+const MAX_FILTER_ATTEMPT = 3;
+interface CommandPanelProps {
+    position: Vector2,
+    commands: string[],
+    onClose: ()=> void
+}
+
+export const CommandPanel = ({position, commands, onClose}: CommandPanelProps)=> {
+
+    const [ commandText, setCommandText ] = useState<string>('');
+    const [ filterAttempt, setFilterAttempt ] = useState(0);
+
+    const filteredCommands = useMemo(()=> {
+        const result = commands.filter(cmd => cmd.includes(commandText));
+        if (result.length === 0) setFilterAttempt(old => old + 1);
+        if (result.length > 0) setFilterAttempt(0); 
+        return commands.filter(cmd => cmd.includes(commandText));
+    }, [commandText])
+
+    const onType = useCallback((ev: KeyboardEvent)=> {
+        if (ev.key === 'Escape') {
+            onClose();
+            return;
+        }
+
+        const element = window.getSelection().focusNode.parentElement;
+
+        if (element.id === 'editorTextArea') {
+            onClose();
+            return;
+        }
+
+        const text = element.innerHTML;
+
+        if (!text.includes('/')) {
+            onClose();
+            return;
+        }
+
+        const allCommands = text.split('/');
+        if (allCommands.length === 0) return;
+
+        const lastCommandText = `/${allCommands[allCommands.length -1]}`;
+
+        if (lastCommandText) {
+            if(lastCommandText) {
+                setCommandText(lastCommandText);
+            }
+        } else {
+            setCommandText('');
+        }
+    }, [setCommandText])
+
+    useEffect(()=> {
+        window.addEventListener('keyup', onType);
+        return ()=> window.removeEventListener('keyup', onType);
+    }, [setCommandText])
+
+    useEffect(()=> {
+        if (filterAttempt >= MAX_FILTER_ATTEMPT) {
+            onClose();
+        }
+    }, [filterAttempt])
+
+    return (
+        <div
+            style={{top: position.y, left: position.x}}
+            className='CommandPanel'
+            >
+            
+            <div className='commandList'>
+                { filteredCommands.map((cmd) => {
+                    return (
+                        <div
+                            key={cmd}
+                            className='commandOption'
+                            >
+                            {cmd}
+                        </div>
+                    )
+                })}
+            </div>
+            
+        </div>
+    )
+}
--- src/components/EditorContainer/EditorContainer.tsx
+++ src/components/EditorContainer/EditorContainer.tsx
@@ -1,12 +1,14 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { BlockContainer } from '../BlockContainer';
-import { EsperBlock } from '../sharedTypes';
+import { CommandPanel } from '../CommandPanel';
+import { EsperBlock, Vector2 } from '../sharedTypes';
 import './EditorContainer.scss';
 
 export const EditorContainer = ()=> {
 
+    const [ showCommandPanel, setShowCommandPanel ] = useState<{position: Vector2} | null>();
     const [ blocks, setBlocks ] = useState<Array<EsperBlock>>([]);
-    const focusOn  = useRef<string | null>(null);
+    const editorRef = useRef<HTMLDivElement>();
 
     const addBlock = ()=> {
         setBlocks((old)=> [...old, { id: Math.random().toString()}])
@@ -16,28 +18,57 @@ export const EditorContainer = ()=> {
         addBlock();
     }
 
-    const OnBlockFocus = (id: string)=> {
-        focusOn.current = id;
+    const openCommandPanel = (position: Vector2) => {
+        setShowCommandPanel({position});
     }
-    
+
+    useEffect(()=> {
+        /** detect when user want to open the command panel */
+        if (editorRef.current) {
+            editorRef.current.addEventListener('keyup', (ev)=> {
+                if (ev.key === '/') {
+                    const element = window.getSelection().focusNode.parentElement;
+                    const position = {x: element.offsetLeft, y: element.offsetTop};
+                    openCommandPanel(position);         
+                }
+            })
+        }
+        
+    }, [editorRef.current])
+
     return (
-        <div
-            className='EditorContainer'
-            onClick={OnClickEditorContainer}
-            >
-            Editor container
-            <div
-                contentEditable={true}
-                suppressContentEditableWarning={true}
-            >
-                { blocks.map((block)=> 
-                    <BlockContainer
-                        key={block.id}
-                        onFocus={OnBlockFocus}
-                        {...block} 
+        <div className='EditorContainer'>
+            { showCommandPanel && 
+                <CommandPanel 
+                    {...showCommandPanel}
+                    commands={['/divider', '/h3']}
+                    onClose={()=> setShowCommandPanel(null)}
                     />
-                )}
+            }
+
+            <div
+                ref={editorRef}
+                onClick={OnClickEditorContainer}
+                >
+                Editor container
+            
+                
+                <div
+                    className='editorTextArea'
+                    id='editorTextArea'
+                    contentEditable={true}
+                    suppressContentEditableWarning={true}
+                    >
+                    { blocks.map((block)=> 
+                        <BlockContainer
+                            key={block.id}
+                            {...block} 
+                        />
+                    )}
+                </div>
+
             </div>
+
         </div>
     )
 }
